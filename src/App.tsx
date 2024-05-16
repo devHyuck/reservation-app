1ab50a5453fdfef8ea6188bace0ad31b3246f148
devHyuck <pacman3164@gmail.com>
2024-05-16 15:37:19 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import GlobalStyle from "./styles/GlobalStyle";
+
+const App = () => {
+	return (
+		<>
+			<GlobalStyle />
+			<Outlet />
+		</>
+	);
+};
+
+export default App;
---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,44 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+	*,
+	*::before,
+	*::after {
+		box-sizing: border-box;
+		margin: 0;
+		padding: 0;
+	}
+
+	html {
+		-moz-text-size-adjust: none;
+		-webkit-text-size-adjust: none;
+		text-size-adjust: none;
+		font-size: 16px;
+	}
+
+	body,
+	h1,
+	h2,
+	h3,
+	h4,
+	p,
+	figure,
+	blockquote,
+	dl,
+	dd {
+		margin-block-end: 0;
+	}
+
+	ul,
+	ol{
+		list-style: none;
+	}
+
+	a {
+		display: block;
+		text-decoration: none;
+		color: inherit;
+	}
+`;
+
+export default GlobalStyle;
